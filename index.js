cdbeaaa157e659dc69295363d94b9c4cd5630952
fajarriskihidayat <fajarrh2002@gmail.com>
2023-11-12 05:44:21 +0700

    setup and connection database

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const PORT = process.env.PORT || 3000;
+
+const db = require("./config/db");
+db.then(() => {
+  console.log("Database connected...");
+}).catch(() => {
+  console.log("Failed connect database");
+});
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
